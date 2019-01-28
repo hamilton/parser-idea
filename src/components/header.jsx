7e97361fe05b3fc17bd699e68e64faba3498b0e0
hamilton <hamilton.ulmer@gmail.com>
2019-01-27 22:58:27 -0800

    update to styling

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -15,20 +15,33 @@ background: #AA076B;  /* fallback for old browsers */
 background: -webkit-linear-gradient(to right, #61045F, #AA076B);  /* Chrome 10-25, Safari 5.1-6 */
 background: linear-gradient(to right, #61045F, #AA076B); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 
-display: grid;
-grid-template-columns: 1fr 1fr 1fr;
+
 margin:auto;
 margin-bottom: 50px;
 padding:15px;
 
-`
 
+
+`
+const second = 'black';
 const Main = styled.div`
 text-shadow: 
 1px 1px 0px #d02000,
 2px 2px  0px #d02000,
 3px 3px  0px #d02000,
 4px 4px  0px #d02000;
+text-align:center;
+
+span {
+    font-size:.7em;
+    opacity: .8;
+    font-weight:100;
+    text-shadow: 
+1px 1px 0px ${second},
+2px 2px  0px ${second},
+3px 3px  0px ${second},
+4px 4px  0px ${second};
+}
 `
 
 const Tagline = styled.div`
@@ -44,8 +57,6 @@ font-family: 'Gill Sans', 'Gill Sans MT', Calibri, 'Trebuchet MS', sans-serif;
 
 export default () => (
   <HeaderContainer>
-    <div />
     <Main>Product Metrics Explorer</Main>
-    <Tagline />
   </HeaderContainer>
 )
